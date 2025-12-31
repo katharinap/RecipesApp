46d549fddc17ea7798f623f327a65582c5e797b4
Katharina Peter <katharina.peter@gmail.com>
2025-12-31 17:36:11 +0100

    disable dynamic color theme

--- app/src/main/java/com/katharina/recipesapp/ui/theme/Theme.kt
+++ app/src/main/java/com/katharina/recipesapp/ui/theme/Theme.kt
@@ -265,7 +265,7 @@ val unspecified_scheme =
 fun RecipesAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme =
